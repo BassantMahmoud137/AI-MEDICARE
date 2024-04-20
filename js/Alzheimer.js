66ab6da946ceecfa3624ff9356b0c125291055b9
bassant <bassantmahmoud137@gmail.com>
2024-04-20 23:18:35 +0200

    ai rabt 2

---
+++ js/Alzheimer.js
@@ -0,0 +1,44 @@
+let resultSection = document.querySelector('#result-section');
+
+document
+  .getElementById('imageForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const fileInput = document.getElementById('imageInput');
+    const file = fileInput.files[0];
+
+    const formData = new FormData();
+    formData.append('image', file);
+
+    fetch('https://3256-34-148-227-120.ngrok-free.app/', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json(); // assuming API returns JSON
+        } else {
+          throw new Error('Error uploading image');
+        }
+      })
+      .then((data) => {
+        // Handle the API response data here
+        // console.log('Predection:', data.prediction);
+        displayResult(data);
+        // Example: Display a success message
+        //   alert('Image uploaded successfully');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // Example: Display an error message
+        alert('Error uploading image');
+      });
+  });
+
+function displayResult(data) {
+  resultSection.classList.remove('d-none');
+  resultSection.innerHTML = `
+    <span class="py-3 mb-3"> Predection: ${data.prediction}</span>
+    `;
+}
