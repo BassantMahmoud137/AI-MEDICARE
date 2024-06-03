4ebd7687302553a3e3f3efdb5b7001e81e8f96f3
bassant <bassantmahmoud137@gmail.com>
2024-06-03 21:51:50 +0300

    ai edits

--- js/Alzheimer.js
+++ js/Alzheimer.js
@@ -11,7 +11,7 @@ document
     const formData = new FormData();
     formData.append('image', file);
 
-    fetch('https://ed0a-34-80-128-47.ngrok-free.app/', {
+    fetch('https://gibbon-skilled-allegedly.ngrok-free.app/', {
       method: 'POST',
       body: formData,
     })
