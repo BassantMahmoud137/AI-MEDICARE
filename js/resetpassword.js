5ddbd56490ef9c5c4c85483d51973ed715a5c5a9
bassant <bassantmahmoud137@gmail.com>
2024-06-03 20:59:22 +0300

    ed

--- js/resetpassword.js
+++ js/resetpassword.js
@@ -1,20 +1,20 @@
-const resetPasswordButton = document.querySelector("#resetPasswordButton")
+//const resetPasswordButton = document.querySelector("#resetPasswordButton")
 
-const resetPassword = async () => {
-    const email = document.querySelector('#email').value;
-    const response = await fetch(`https://ai-medicare.onrender.com/api/users/forgotPassword`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-    });
-    const data = await response.json();
-    if (response.ok) {
-        window.location ="ResetPassword.html";
-    } else {
-        alert(data.message);
-    }
-}
+//const resetPassword = async () => {
+   // const email = document.querySelector('#email').value;
+   // const response = await fetch(`https://ai-medicare.onrender.com/api/users/forgotPassword`, {
+    //    method: 'POST',
+     //   headers: {
+           // 'Content-Type': 'application/json',
+       // },
+       // body: JSON.stringify({ email }),
+    //});
+    //const data = await response.json();
+    //if (response.ok) {
+      //  window.location ="ResetPassword.html";
+    //} else {
+    //    alert(data.message);
+    //}
+//}
 
-resetPasswordButton.addEventListener('click', resetPassword)
+//resetPasswordButton.addEventListener('click', resetPassword)
